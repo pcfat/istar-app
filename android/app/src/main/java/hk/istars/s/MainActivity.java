df3ae673deae0fb482fae4b0a8e5cca603cd01e7
Poon Chun Fat <poonchunfat@PoondeMacBook-Pro.local>
2026-05-02 17:49:47 +0800

    Fix missing Intent import

--- android/app/src/main/java/hk/istars/s/MainActivity.java
+++ android/app/src/main/java/hk/istars/s/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
